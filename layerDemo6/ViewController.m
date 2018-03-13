96824dadba5669f17280bfeefaabfbfb6276de9c
浅爱 <shallowlove21@gmail.com>
2018-03-13 11:06:25 +0800

    commit

--- layerDemo6/ViewController.m
+++ layerDemo6/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView     *layerView;
+@property (nonatomic) CALayer                   *blueLayer;
+@property (nonatomic) UIAlertController         *alertVC;
 
 @end
 
@@ -16,13 +19,66 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.blueLayer = [CALayer layer];
+    self.blueLayer.frame = CGRectMake(130.0f, 300.0f, 100.0f, 100.0f);
+    self.blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    
+    [self.layerView.layer addSublayer:self.blueLayer];
+ 
+    
 }
 
+//- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+//
+//    CGPoint point = [[touches anyObject] locationInView:self.view];
+//
+//    point = [self.layerView.layer convertPoint:point fromLayer:self.view.layer];
+//
+//    if ([self.layerView.layer containsPoint:point] ) {
+//
+//        point = [self.blueLayer convertPoint:point fromLayer:self.layerView.layer];
+//
+//    }
+//
+//    if ([self.blueLayer containsPoint:point]) {
+//        _alertVC = [UIAlertController alertControllerWithTitle:@"点击了蓝色layer" message:nil preferredStyle:UIAlertControllerStyleAlert];
+//
+//    }else {
+//        _alertVC = [UIAlertController alertControllerWithTitle:@"点击了白色的layer" message:nil preferredStyle:UIAlertControllerStyleAlert];
+//        }
+//
+//    UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+//        NSLog(@"取消");
+//
+//    }];
+//
+//        [_alertVC addAction:action];
+//
+//        [self presentViewController:_alertVC animated:YES completion:nil];
+//
+//}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    CGPoint potin = [[touches anyObject] locationInView:self.layerView];//1self.view
+    
+    CALayer *touchedLayer = [self.layerView.layer hitTest:potin];
+    
+    if (touchedLayer == self.blueLayer) {
+        _alertVC = [UIAlertController alertControllerWithTitle:@"点击了蓝色layer" message:nil preferredStyle:UIAlertControllerStyleAlert];
+    }else if (touchedLayer == self.layerView.layer) {
+        _alertVC = [UIAlertController alertControllerWithTitle:@"点击了白色的layer" message:nil preferredStyle:UIAlertControllerStyleAlert];
+    }
+    
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"取消");
+    }];
+    
+    [_alertVC addAction:action];
+    
+    [self presentViewController:_alertVC animated:YES completion:nil];
+    
 }
 
 
